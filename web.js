7d3e6421828a7daaedc84bfb4ee0ea033f3170ed
Vishal Srivastava <vishalzone2002@gmail.com>
2013-07-11 02:58:48 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+buf = new Buffer(256);
+buf = fs.readFile('/home/ubuntu/bitstarter/index.html')
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
